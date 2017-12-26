f2950d69a55864f61b24a45890bc25db2a3cedac
Coteh <james_c_34@live.com>
2017-12-26 18:33:54 -0500

    Add delete image feature

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,3 +1,5 @@
+import { setTimeout } from "timers";
+
 var commentCount = 0;
 
 var updateCommentsCounter = function() {
@@ -28,12 +30,12 @@ var onCommentSubmitted = function() {
     var submittedComment;
     var parentElement = document.querySelector("#comments-container #comments");
     if (this.status !== 200) {
-        jsonObj = JSON.parse(this.responseText);
+        var jsonObj = JSON.parse(this.responseText);
         showNotification(jsonObj.message, {
             error: true
         });
     } else {
-        jsonObj = JSON.parse(this.responseText);
+        var jsonObj = JSON.parse(this.responseText);
         submittedComment = jsonObj.message;
         commentCount++;
         updateCommentsCounter();
@@ -44,11 +46,45 @@ var onCommentSubmitted = function() {
     convertTimeElementsToLocalTime(parentElement);
 };
 
+var onImageDeleted = function() {
+    var jsonObj = JSON.parse(this.responseText);
+    var html = "<div>" + jsonObj.message + "</div>"
+    showOverlay(html, {
+        error: (this.status === 500),
+        close: (this.status === 500)
+    });
+    if (this.status === 200) {
+        setTimeout(function() {
+            window.location.href = window.location.origin + "/";
+        }, 2000);
+    }
+};
+
 window.requestComments = function(imageID) {
     var req = new XMLHttpRequest();
     req.onload = onCommentsLoaded;
     req.open("get", "/images/" + imageID + "/comments?type=json&responseType=html");
     req.send();
+};
+
+window.deleteImage = function (imageID) {
+    var req = new XMLHttpRequest();
+    req.onload = onImageDeleted;
+    req.open("delete", "/images/" + imageID + "?type=json");
+    req.send();
+};
+
+window.confirmDeleteImage = function(imageID) {
+    var html = "<div id='delete-confirm'>"
+        + "Are you sure you want to delete this image? "
+        + "This action cannot be undone."
+        + "<div id='delete-confirm-yesno'>"
+        + "<button onclick='deleteImage(" + imageID + ");'>Yes</button>"
+        + "<button onclick='clearOverlay();'>Cancel</button>"
+        + "</div></div>";
+    showOverlay(html, {
+        close: true
+    });
 };
 
 var submitComment = function() {
