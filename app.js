3e6beeca09e123e92545253a39c86ce277d3d849
Coteh <james_c_34@live.com>
2017-12-29 23:10:25 -0500

    Remove config collection, top ID, and config functions

--- app.js
+++ app.js
@@ -10,32 +10,9 @@ ImageDatabase.startDatabaseClient(function(err) {
         rootDirName: __dirname
     });
     Server.runServer(9001, function(err) {
-        var dbConfigErr = false;
-        ImageDatabase.getConfigFromDatabase(function(err, config) {
-            if (err) {
-                console.log("There was an error retrieving config options from the database.");
-                dbConfigErr = true;
-                return;
-            }
-            if (typeof(config) === "undefined") {
-                console.log("No config options currently exist in database, instantiating a fresh set and adding to database...");
-                ImageDatabase.insertNewConfig();
-            }
-            console.log("Here are the current config options:");
-            ImageDatabase.printCurrentConfig();
-            console.log("Now applying them to the server instance...");
-            ImageDatabase.applyConfigOptions();
-        });
-        if (dbConfigErr) {
-            console.log("ERROR: DB configs could not be loaded. Disconnecting server and connection to db and exiting...");
-            ImageDatabase.closeDatabaseClient();
-            Server.closeServer();
-            return;
-        }
-        if (process.argv.length > 2) {
-            if (process.argv[2] == "print-config") {
-                ImageDatabase.printCurrentConfig();
-            }
+        if (err) {
+            console.error("Could not run server due to an error:");
+            console.error(err);
         }
     });
 });
