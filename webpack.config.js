30ab5e641702391986c7d05cc80cd49e085b7c5a
Coteh <james_c_34@live.com>
2017-12-29 23:30:55 -0500

    Add util-frontend.js to home webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: {
-        'assets/js/home.bundle': ['./assets/js/main.js', './assets/js/file-upload.js'],
+        'assets/js/home.bundle': ['./assets/js/main.js', './assets/js/file-upload.js', './assets/js/util-frontend.js'],
         'assets/js/image.bundle': ['./assets/js/main.js', './assets/js/image.js', './assets/js/util-frontend.js'],
         'assets/js/user.bundle': ['./assets/js/main.js', './assets/js/user.js', './assets/js/util-frontend.js']
     },
