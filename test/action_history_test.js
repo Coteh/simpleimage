5392a185afb9cd746c5430b7383d28e6833533be
Coteh <james_c_34@live.com>
2018-05-10 01:36:19 -0400

    Add failing tests for action history

---
+++ test/action_history_test.js
@@ -0,0 +1,17 @@
+var actionHistory = require("../lib/action-history");
+
+describe("action history", function() {
+    describe("writeActionHistoryEntry", function() {
+        it("should write an action history entry with all the required information", function(done) {
+            assert.fail("Not implemented");
+        });
+
+        it("should produce an error if required information for an action history is not provided", function(done) {
+            assert.fail("Not implemented");
+        });
+
+        it("should provide an error if undefined is passed as any of the arguments", function(done) {
+            assert.fail("Not implemented");
+        });
+    });
+});
