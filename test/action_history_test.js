36209a72c83aae7b8323064e3df80653a9e468a6
Coteh <james_c_34@live.com>
2018-05-10 14:02:03 -0400

    Stub out action history dep for testing using proxyquire

--- test/action_history_test.js
+++ test/action_history_test.js
@@ -1,9 +1,51 @@
-var actionHistory = require("../lib/action-history");
+var assert = require("assert");
+var proxyquire = require("proxyquire");
+
+var databaseOpsStub = {
+    writeActionHistoryEntry: function(actionEntry, callback) {
+        var testObj = {
+            type: actionEntry.type,
+            item: actionEntry.item,
+            username: actionEntry.username,
+            ip_address: actionEntry.ipAddress,
+            info: actionEntry.info,
+            action_date: new Date(0)
+        };
+
+        callback(null, testObj);
+    }
+};
+
+var actionHistory = proxyquire("../lib/action-history", { "./database-ops": databaseOpsStub });
 
 describe("action history", function() {
     describe("writeActionHistoryEntry", function() {
         it("should write an action history entry with all the required information", function(done) {
-            assert.fail("Not implemented");
+            var testData = {
+                type: "UPLOAD_IMAGE",
+                item: "ImageID",
+                username: "si_user",
+                ipAddress: "127.0.0.1",
+                info: {
+                    request_url: "upload",
+                    author: "si_user"
+                }
+            };
+            actionHistory.writeActionHistory(testData, function (err, result) {
+                if (err) {
+                    assert.fail(err);
+                    done();
+                    return;
+                }
+                assert.ifError(err);
+                assert.ok(result);
+                assert.equal(result.type, testData.type);
+                assert.equal(result.item, testData.item);
+                assert.equal(result.username, testData.username);
+                assert.equal(result.ip_address, testData.ipAddress);
+                assert.equal(result.info, testData.info);
+                done();
+            });
         });
 
         it("should produce an error if required information for an action history is not provided", function(done) {
