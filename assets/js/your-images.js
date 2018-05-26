40ec824cef02c547ad71e2e301a6f24ada77e2d7
Coteh <james_c_34@live.com>
2018-05-26 14:38:22 -0400

    Bind a model containing image IDs to the selectable UI

--- assets/js/your-images.js
+++ assets/js/your-images.js
@@ -1,5 +1,14 @@
-var deleteImages = function(imageIDs) {
-    console.log("TODO");
+window.YourImages = {
+    imageIDs: []
+};
+
+window.deleteImages = function(imageIDs) {
+    // TODO Perform delete call here
+    if (imageIDs.length === 0) {
+        console.error("No images selected for deletion.");
+        return;
+    }
+    console.log("Deleting " + imageIDs.toString() + "...");
 };
 
 $(function() {
@@ -9,11 +18,21 @@ $(function() {
             + "Are you sure you want to delete the selected images? "
             + "This action cannot be undone."
             + "<div id='delete-confirm-yesno'>"
-            + "<span class='button' onclick='deleteImages(\"" + [] + "\");'>Yes</span>"
+            + "<span class='button' onclick='deleteImages(\"" + YourImages.imageIDs + "\");'>Yes</span>"
             + "<span class='button' onclick='clearOverlay();'>Cancel</span>"
             + "</div></div>";
         showOverlay(html, {
             close: true
         });
+    });
+    SelectableActions.addOnSelectedListener(function(selectable) {
+        YourImages.imageIDs = YourImages.imageIDs.concat(selectable.dataset.imageId);
+        console.log(YourImages.imageIDs);
+    });
+    SelectableActions.addOnDeselectedListener(function (selectable) {
+        YourImages.imageIDs = YourImages.imageIDs.filter(function(value) {
+            return (value !== selectable.dataset.imageId);
+        });
+        console.log(YourImages.imageIDs);
     });
 });
