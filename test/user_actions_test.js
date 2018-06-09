23fb9678e1aa1b4ef78eb0ad89d25c2f60e8dacb
Coteh <james_c_34@live.com>
2018-06-09 14:10:24 -0400

    Update user actions tests

--- test/user_actions_test.js
+++ test/user_actions_test.js
@@ -2,29 +2,29 @@ const assert = require("assert");
 const userActions = require("../lib/user-actions");
 const ObjectID = require("mongodb").ObjectID;
 
-var getRegisteredUserSession = function() {
+var getRegisteredUserSession = function (username = "testuser") {
     return {
         user: {
-            username: "james",
-            email: "james@james.com"
+            username,
+            email: username + "@email.com"
         }
     };
 };
 
-var getUnregisteredUserSession = function() {
+var getUnregisteredUserSession = function (unregisteredSessionID = "zxcvbnm") {
     return {
-        unregisteredSessionID: "qwertyuiop"
+        unregisteredSessionID
     };
 };
 
-var getRegisteredUserSessionWithUnregisteredSession = function() {
-    return Object.assign(getUnregisteredUserSession(), getRegisteredUserSession());
+var getRegisteredUserSessionWithUnregisteredSession = function(username, unregisteredSessionID) {
+    return Object.assign(getUnregisteredUserSession(unregisteredSessionID), getRegisteredUserSession(username));
 };
 
 describe("user actions", function() {
     describe("compareUserImageAuthorization", function () {
         it("should return true if username on image is equal to username on session", function () {
-            var session = getRegisteredUserSession();
+            var session = getRegisteredUserSession("james");
             var testImage = {
                 _id: new ObjectID("".toString().padStart(24, "a")),
                 encoding: "7bit",
@@ -37,7 +37,7 @@ describe("user actions", function() {
             assert.equal(userActions.compareUserImageAuthorization(session, testImage), true);
         });
         it("should return false if username on image is not equal to username on session", function () {
-            var session = getRegisteredUserSession();
+            var session = getRegisteredUserSession("james");
             var testImage = {
                 _id: new ObjectID("".toString().padStart(24, "a")),
                 encoding: "7bit",
@@ -50,7 +50,7 @@ describe("user actions", function() {
             assert.equal(userActions.compareUserImageAuthorization(session, testImage), false);
         });
         it("should return true if unregistered session ID on image is equal to unregistered session ID on session", function () {
-            var session = getUnregisteredUserSession();
+            var session = getUnregisteredUserSession("qwertyuiop");
             var testImage = {
                 _id: new ObjectID("".toString().padStart(24, "a")),
                 encoding: "7bit",
@@ -64,7 +64,7 @@ describe("user actions", function() {
             assert.equal(userActions.compareUserImageAuthorization(session, testImage), true);
         });
         it("should return false if unregistered session ID on image is not equal to unregistered session ID on session", function () {
-            var session = getUnregisteredUserSession();
+            var session = getUnregisteredUserSession("qwertyuiop");
             var testImage = {
                 _id: new ObjectID("".toString().padStart(24, "a")),
                 encoding: "7bit",
@@ -78,7 +78,7 @@ describe("user actions", function() {
             assert.equal(userActions.compareUserImageAuthorization(session, testImage), false);
         });
         it("should return false if a unregistered user session is used to verify operation on an image uploaded by a registered user", function () {
-            var session = getUnregisteredUserSession();
+            var session = getUnregisteredUserSession("qwertyuiop");
             var testImage = {
                 _id: new ObjectID("".toString().padStart(24, "a")),
                 encoding: "7bit",
@@ -91,7 +91,7 @@ describe("user actions", function() {
             assert.equal(userActions.compareUserImageAuthorization(session, testImage), false);
         });
         it("should return false if a registered user session is used to verify operation on an image uploaded anonymously", function () {
-            var session = getRegisteredUserSession();
+            var session = getRegisteredUserSession("james");
             var testImage = {
                 _id: new ObjectID("".toString().padStart(24, "a")),
                 encoding: "7bit",
@@ -106,7 +106,7 @@ describe("user actions", function() {
         });
         it("should return true if a registered user session with unregistered session ID is used to verify operation "
             + "on an image uploaded anonymously with same unregistered session ID", function () {
-            var session = getRegisteredUserSessionWithUnregisteredSession();
+                var session = getRegisteredUserSessionWithUnregisteredSession("james", "qwertyuiop");
             var testImage = {
                 _id: new ObjectID("".toString().padStart(24, "a")),
                 encoding: "7bit",
@@ -129,7 +129,6 @@ describe("user actions", function() {
                 id: "abcdef",
                 username: "james"
             };
-            var session = getRegisteredUserSession();
             assert.throws(function() {
                 userActions.compareUserImageAuthorization(undefined, testImage);
             }, function(err) {
@@ -146,7 +145,6 @@ describe("user actions", function() {
                 id: "abcdef",
                 username: "james"
             };
-            var session = getRegisteredUserSession();
             assert.throws(function() {
                 userActions.compareUserImageAuthorization(null, testImage);
             }, function(err) {
@@ -171,8 +169,8 @@ describe("user actions", function() {
         });
     });
     describe("authorizeUserImageOperation", function() {
-        it("users can operate on their own image", function(done) {
-            var session = getRegisteredUserSession();
+        it("users can operate on their own image", function (done) {
+            var session = getRegisteredUserSession("james");
             var testImage = {
                 _id: new ObjectID("".toString().padStart(24, "a")),
                 encoding: "7bit",
@@ -189,101 +187,124 @@ describe("user actions", function() {
                 })
                 .catch(function (err) {
                     assert.fail(err.message);
+                    done();
                 });
         });
-        it("users cannot operate on other users' image", function() {
-            assert.fail("Not implemented");
+        it("users cannot operate on other users' image", function (done) {
+            var session = getRegisteredUserSession("james");
+            var testImage = {
+                _id: new ObjectID("".toString().padStart(24, "a")),
+                encoding: "7bit",
+                uploadeddate: new Date(0),
+                data: new Buffer(0),
+                mimetype: "image/png",
+                id: "abcdef",
+                username: "jake"
+            };
+            userActions.authorizeUserImageOperation(session, testImage)
+                .then(function (image) {
+                    assert.fail("User was able to operate on another user's image, but they shouldn't be able to in this case.");
+                    done();
+                })
+                .catch(function (err) {
+                    assert.ok(err);
+                    done();
+                });
         });
-        it("guests can operate on their own image", function() {
-            assert.fail("Not implemented");
+        it("guests can operate on their own image", function (done) {
+            var session = getUnregisteredUserSession("qwertyuiop");
+            var testImage = {
+                _id: new ObjectID("".toString().padStart(24, "a")),
+                encoding: "7bit",
+                uploadeddate: new Date(0),
+                data: new Buffer(0),
+                mimetype: "image/png",
+                id: "abcdef",
+                username: null,
+                unregisteredSessionID: "qwertyuiop"
+            };
+            userActions.authorizeUserImageOperation(session, testImage)
+                .then(function (image) {
+                    assert.ok(image);
+                    done();
+                })
+                .catch(function (err) {
+                    assert.fail(err.message);
+                    done();
+                });
         });
-        it("guests cannot operate on other guests' image", function() {
-            assert.fail("Not implemented");
+        it("guests cannot operate on other guests' image", function (done) {
+            var session = getUnregisteredUserSession("qwertyuiop");
+            var testImage = {
+                _id: new ObjectID("".toString().padStart(24, "a")),
+                encoding: "7bit",
+                uploadeddate: new Date(0),
+                data: new Buffer(0),
+                mimetype: "image/png",
+                id: "abcdef",
+                username: null,
+                unregisteredSessionID: "asdfghjkl"
+            };
+            userActions.authorizeUserImageOperation(session, testImage)
+                .then(function (image) {
+                    assert.fail("Guest was able to operate on another guest's image, but they shouldn't be able to in this case.");
+                    done();
+                })
+                .catch(function (err) {
+                    assert.ok(err);
+                    done();
+                });
         });
-        it("guests cannot operate on a registered user's image", function() {
-            assert.fail("Not implemented");
+        it("guests cannot operate on a registered user's image", function (done) {
+            var session = getUnregisteredUserSession("qwertyuiop");
+            var testImage = {
+                _id: new ObjectID("".toString().padStart(24, "a")),
+                encoding: "7bit",
+                uploadeddate: new Date(0),
+                data: new Buffer(0),
+                mimetype: "image/png",
+                id: "abcdef",
+                username: "james"
+            };
+            userActions.authorizeUserImageOperation(session, testImage)
+                .then(function (image) {
+                    assert.fail("Guest was able to operate on a registered user's image, but they shouldn't be able to in this case.");
+                    done();
+                })
+                .catch(function (err) {
+                    assert.ok(err);
+                    done();
+                });
         });
-        it("registered user's cannot operate on guests' image (guests besides themselves)", function() {
-            assert.fail("Not implemented");
-        });
-        it("registered users can operate on image they uploaded as a guest", function() {
-            assert.fail("Not implemented");
-        });
-        it("should throw if session is undefined", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if session is null", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if image is undefined", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if image is null", function () {
-            assert.fail("Not implemented");
-        });
+        it("registered user's cannot operate on guests' image (guests besides themselves)");
+        it("registered users can operate on image they uploaded as a guest");
+        it("should throw if session is undefined");
+        it("should throw if session is null");
+        it("should throw if image is undefined");
+        it("should throw if image is null");
     });
     describe("authorizeUserMultiImageOperation", function () {
-        it("users can operate on their own images", function () {
-            assert.fail("Not implemented");
-        });
-        it("users cannot operate on other users' images", function () {
-            assert.fail("Not implemented");
-        });
-        it("guests can operate on their own images", function () {
-            assert.fail("Not implemented");
-        });
-        it("guests cannot operate on other guests' images", function () {
-            assert.fail("Not implemented");
-        });
-        it("guests cannot operate on a registered user's images", function () {
-            assert.fail("Not implemented");
-        });
-        it("registered user's cannot operate on guests' images (guests besides themselves)", function () {
-            assert.fail("Not implemented");
-        });
-        it("registered users can operate on images they uploaded as a guest", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if session is undefined", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if session is null", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if image is undefined", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if image is null", function () {
-            assert.fail("Not implemented");
-        });
+        it("users can operate on their own images");
+        it("users cannot operate on other users' images");
+        it("guests can operate on their own images");
+        it("guests cannot operate on other guests' images");
+        it("guests cannot operate on a registered user's images");
+        it("registered user's cannot operate on guests' images (guests besides themselves)");
+        it("registered users can operate on images they uploaded as a guest");
+        it("should throw if session is undefined");
+        it("should throw if session is null");
+        it("should throw if image is undefined");
+        it("should throw if image is null");
     });
     describe("transferGuestImageToUser", function () {
-        it("an image owned by an unregistered user can be transferred to a registered user successfully", function () {
-            assert.fail("Not implemented");
-        });
-        it("an image transferred from unregistered user to a registered user shall have its unregistered session ID removed", function () {
-            assert.fail("Not implemented");
-        });
-        it("should not allow an image owned by another registered user to transfer to a registered user", function () {
-            assert.fail("Not implemented");
-        });
-        it("should not allow an image owned by an unregistered user to be transferred to another unregistered user", function () {
-            assert.fail("Not implemented");
-        });
-        it("image transfer failures should not remove the unregistered session ID link", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if an undefined image is passed in", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if a null image is passed in", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if an undefined user is passed in", function () {
-            assert.fail("Not implemented");
-        });
-        it("should throw if a null user is passed in", function () {
-            assert.fail("Not implemented");
-        });
+        it("an image owned by an unregistered user can be transferred to a registered user successfully");
+        it("an image transferred from unregistered user to a registered user shall have its unregistered session ID removed");
+        it("should not allow an image owned by another registered user to transfer to a registered user");
+        it("should not allow an image owned by an unregistered user to be transferred to another unregistered user");
+        it("image transfer failures should not remove the unregistered session ID link");
+        it("should throw if an undefined image is passed in");
+        it("should throw if a null image is passed in");
+        it("should throw if an undefined user is passed in");
+        it("should throw if a null user is passed in");
     });
 });
