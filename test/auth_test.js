3eac2d5b346084361c617b7c4b7d4a5cd2abda97
Coteh <james_c_34@live.com>
2019-10-25 23:23:28 -0400

    Add tests and update db-ops with new auth module

--- test/auth_test.js
+++ test/auth_test.js
@@ -1,14 +1,96 @@
 const assert = require("assert");
 const auth = require("../lib/auth");
 
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+
 const REGULAR_PASSWORD = "myPassword";
 const REGULAR_PASSWORD_HASHED = "$2a$10$LgUwc4WQBxjoAQYM/jxha.7nT2m5yMYd6PHeVn.4VYuu86JCjdPqC";
-const BCRYPT_CUTOFF_SAMPLE = "abcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefg";
+const BCRYPT_CUTOFF_SAMPLE = "abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef";
 const LONG_PASSWORD = BCRYPT_CUTOFF_SAMPLE + "A";
-const LONG_PASSWORD_HASHED = "$2a$10$YiuBDJeubPLYin3TknOEbuUXkiUcjPRmKNesaubst8lXSHZ2xDDwK";
+const LONG_PASSWORD_HASHED = "$2a$10$XRrOYig1EBlIwfmKaIen5uM17YOc0E4yXR/o6rDzcK0gr01pIUhPq";
 const ALT_LONG_PASSWORD = BCRYPT_CUTOFF_SAMPLE + "D";
 
+// The same as preHashPassword in auth module, to be used for hashPassword tests
+var preHashPassword = function(password) {
+    let hash = crypto.createHash("sha512");
+    hash.update(password, "utf8");
+    return hash.digest("utf8");
+}
+
 describe("auth", function () {
+    describe("hashPassword", function() {
+        it("should create an appropriate hash for given user password", function() {
+            var testPassword = REGULAR_PASSWORD;
+            return new Promise((resolve, reject) => {
+                auth.hashPassword(testPassword)
+                    .then((hashedPassword) => {
+                        resolve(hashedPassword);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            }).then((hashedPassword) => {
+                let testPasswordPreHashed = preHashPassword(testPassword);
+                assert.ok(bcrypt.compareSync(testPasswordPreHashed, hashedPassword));
+            });
+        });
+        it("should create a hash of a long password (>72 chars) that cannot be matched with a hash of another >72 char password with same first 72 chars", function() {
+            var testPassword = LONG_PASSWORD;
+            return new Promise((resolve, reject) => {
+                auth.hashPassword(testPassword)
+                    .then((hashedPassword) => {
+                        resolve(hashedPassword);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            }).then((hashedPassword) => {
+                let testPasswordPreHashed = preHashPassword(ALT_LONG_PASSWORD);
+                assert.equal(bcrypt.compareSync(testPasswordPreHashed, hashedPassword), false);
+            });
+        });
+        it("should not create a hash if password is not a string", function () {
+            var testPassword = 6;
+            return new Promise((resolve, reject) => {
+                auth.hashPassword(testPassword)
+                    .then(() => {
+                        reject(new Error("Password should not have been hashed"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Cannot hash non-string password");
+            });
+        });
+        it("should not create a hash if password is undefined", function () {
+            return new Promise((resolve, reject) => {
+                auth.hashPassword(undefined)
+                    .then(() => {
+                        reject(new Error("Password should not have been hashed"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Cannot hash non-string password");
+            });
+        });
+        it("should not create a hash if password is null", function () {
+            return new Promise((resolve, reject) => {
+                auth.hashPassword(null)
+                    .then(() => {
+                        reject(new Error("Password should not have been hashed"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Cannot hash non-string password");
+            });
+        });
+    });
     describe("authenticateUser", function() {
         it("should authenticate the user with a given password and matching hash", function() {
             var testPassword = REGULAR_PASSWORD;
@@ -59,7 +141,7 @@ describe("auth", function () {
                 assert.strictEqual(err.message, "Could not login user. Username and password combination not found.");
             });
         });
-        it("should authenticate a user with a long password past bcrypt's limitations", function() {
+        it("should authenticate a user with a long password past bcrypt's limitations (72 chars)", function() {
             var testPassword = LONG_PASSWORD;
             var user = {
                 password: LONG_PASSWORD_HASHED
@@ -75,13 +157,113 @@ describe("auth", function () {
             });
         })
         it("should fail to authenticate the user if the password is not a string", function() {
-            assert.fail("Not yet implemented");
+            var testPassword = 6;
+            var user = {
+                password: LONG_PASSWORD_HASHED
+            };
+            return new Promise((resolve, reject) => {
+                auth.authenticateUser(user, testPassword)
+                    .then(() => {
+                        reject(new Error("This user should not be authenticated"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Could not login user. Invalid password value provided.");
+            });
         });
         it("should fail to authenticate the user if the password is undefined", function () {
-            assert.fail("Not yet implemented");
+            var user = {
+                password: LONG_PASSWORD_HASHED
+            };
+            return new Promise((resolve, reject) => {
+                auth.authenticateUser(user, undefined)
+                    .then(() => {
+                        reject(new Error("This user should not be authenticated"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Could not login user. Invalid password value provided.");
+            });
         });
         it("should fail to authenticate the user if the password is null", function () {
-            assert.fail("Not yet implemented");
+            var user = {
+                password: LONG_PASSWORD_HASHED
+            };
+            return new Promise((resolve, reject) => {
+                auth.authenticateUser(user, null)
+                    .then(() => {
+                        reject(new Error("This user should not be authenticated"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Could not login user. Invalid password value provided.");
+            });
+        });
+        it("should fail to authenticate the user if user reference is undefined", function() {
+            var testPassword = REGULAR_PASSWORD;
+            return new Promise((resolve, reject) => {
+                auth.authenticateUser(undefined, testPassword)
+                    .then(() => {
+                        reject(new Error("This user should not be authenticated"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Could not login user. Invalid user value provided.");
+            });
+        });
+        it("should fail to authenticate the user if user reference is null", function () {
+            var testPassword = REGULAR_PASSWORD;
+            return new Promise((resolve, reject) => {
+                auth.authenticateUser(null, testPassword)
+                    .then(() => {
+                        reject(new Error("This user should not be authenticated"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Could not login user. Invalid user value provided.");
+            });
+        });
+        it("should fail to authenticate the user if user reference does not contain a 'password' field", function () {
+            var testPassword = REGULAR_PASSWORD;
+            var user = {};
+            return new Promise((resolve, reject) => {
+                auth.authenticateUser(user, testPassword)
+                    .then(() => {
+                        reject(new Error("This user should not be authenticated"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Could not login user. Invalid user value provided.");
+            });
+        });
+        it("should fail to authenticate the user if user reference's 'password' field is not a string", function () {
+            var testPassword = REGULAR_PASSWORD;
+            var user = {
+                password: 6
+            };
+            return new Promise((resolve, reject) => {
+                auth.authenticateUser(user, testPassword)
+                    .then(() => {
+                        reject(new Error("This user should not be authenticated"));
+                    })
+                    .catch((err) => {
+                        resolve(err);
+                    });
+            }).then((err) => {
+                assert.strictEqual(err.message, "Could not login user. Invalid user value provided.");
+            });
         });
     });
 });
