341df3435696e13df276390d51568c6b8bb4aba2
James Cote <3276350+Coteh@users.noreply.github.com>
2021-07-03 21:19:54 -0400

    Add username check to signup form (#257)
    * Add failing tests for check username functionality
    
    * Add route for check_username and pass tests
    
    * Add frontend for username check
    
    * Remove unnecessary deps from check_username_integ_test.js
    
    * Add username length check to /check_username endpoint
    
    - Add message on frontend when username check returns long username error
    - Add message when username is available
    - Extract username check from database-ops into a separate method, to be used in /register route handler and in /check_username. Ideally controllers should be calling the method, but for now I think having the check be called in route handler is better than in DB method.
    - Remove sanitize username and email calls in addUser, which are now useless. Sanitize method to be dealt with in #95
    - Add a couple new test cases to check_username to handle username too long and undefined error with username verification
    
    * Update assets/js/main.js
    
    * Add mongo-sanitize library to sanitize username when passing it to findUser
    
    - Also add test to check_username to ensure that setting up a MongoDB query with $ character will not work
    
    * Add an additional check in check_username route to ensure username passed in is a string specifically
    
    * Handle multiple usernames passed into check_username - only accept the first one passed.
    
    I believe the LGTM check here https://lgtm.com/projects/g/Coteh/simpleimage/rev/pr-f9f55bc57545265fe0b92210ec390456e24bab06 is a false positive. Previous commit prevents arrays from reaching the verifyUsername call. Nonetheless, I have also added this extra handle after looking into this further. I think it would make sense to only check the first username passed in, if multiple were passed in. User should not be passing in multiple usernames ideally, though.
    
    * Fix running app with inspector not starting the server.
    
    --inspect-brk, unlike --inspect, breaks on start of execution of the script. Seems like this was preventing execution of the line that starts the server. By changing it to --inspect, things are working as expected now.
    
    * Add an additional check to verifyUsername to ensure that only strings are passed into it. Also rename the function to isValidUsername and add unit tests.

---
+++ test/integ/check_username_integ_test.js
@@ -0,0 +1,173 @@
+const chai = require("chai");
+const chaiHTTP = require("chai-http");
+const { stub } = require("sinon");
+const databaseOps = require("../../lib/database-ops");
+const usernameUtil = require("../../lib/util/username");
+const server = require("../../lib/server");
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const { MongoClient } = require("mongodb");
+const { assert } = chai;
+
+chai.use(chaiHTTP);
+
+// TODO:#119 shut down mongo mem server and remove --exit hopefully
+
+function getServerAgent() {
+    return chai.request.agent(server.app);
+}
+
+describe("integ", () => {
+    describe("check username", () => {
+        var mongod = null;
+        var db = null;
+        var agent = null;
+        var usersCollection = null;
+        const TEST_USER = "test-user";
+
+        function checkUsername(username) {
+            return agent.get("/check_username")
+                .query({
+                    username,
+                });
+        }
+
+        before(async function () {
+            mongod = new MongoMemoryServer();
+            await mongod.getUri();
+            await mongod.getPort();
+            await mongod.getDbPath();
+            await mongod.getDbName();
+            const testDBURL = mongod.getInstanceInfo().uri;
+            db = await MongoClient.connect(testDBURL);
+            return databaseOps.startDatabaseClient(function (err) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+            }, {
+                dbURL: testDBURL
+            });
+        });
+        beforeEach((done) => {
+            databaseOps.addUser({
+                username: TEST_USER,
+                password: "test",
+                email: "test@test.com"
+            }, () => {
+                agent = getServerAgent();
+                done();
+            });
+        });
+
+        it("should report that a username is taken if it already exists", () => {
+            return checkUsername(TEST_USER)
+                .then(res => {
+                    assert.equal(res.statusCode, 200);
+                    const message = res.body.message;
+                    assert.isTrue(message.exists);
+                });
+        });
+
+        it("should report that a username is available if it's free", () => {
+            return checkUsername("bob")
+                .then(res => {
+                    assert.equal(res.statusCode, 200);
+                    const message = res.body.message;
+                    assert.isFalse(message.exists);
+                });
+        });
+
+        it("should return an error if database error occurs when processing the request", () => {
+            const findUserStub = stub(databaseOps, "findUser").callsArgWith(1, new Error("Error finding user"), null);
+            return checkUsername(TEST_USER)
+                .then(res => {
+                    assert.equal(res.statusCode, 500);
+                    assert.equal(res.body.errorID, "errorCheckingUser");
+                })
+                .finally(err => {
+                    findUserStub.restore();
+                });
+        });
+
+        // TODO test rate limiting of check_username route.
+        // I believe this will require updating configuration for express-rate-limit in route.js
+
+        it("should return an error if there was no user supplied to the request", () => {
+            return checkUsername(undefined)
+                .then(res => {
+                    assert.equal(res.statusCode, 400);
+                    assert.equal(res.body.errorID, "noUsernameToCheck");
+                });
+        });
+
+        it("should return an error if number of characters for username is over the limit", () => {
+            const oldMaxUsernameLength = process.env.MAX_USERNAME_LENGTH;
+            process.env.MAX_USERNAME_LENGTH = 24;
+            return checkUsername("qwertyuiopasdfghjklzxcvbnm")
+                .then(res => {
+                    assert.equal(res.statusCode, 400);
+                    assert.equal(res.body.errorID, "usernameTooLong")
+                })
+                .finally(() => process.env.MAX_USERNAME_LENGTH = oldMaxUsernameLength);
+        });
+
+        it("should return an error if unknown username verification error occurred", () => {
+            const usernameCheckStub = stub(usernameUtil, "isValidUsername").returns({
+                valid: false,
+                error: "",
+            });
+            return checkUsername(TEST_USER)
+                .then(res => {
+                    assert.equal(res.statusCode, 400);
+                    assert.equal(res.body.errorID, "usernameUnknownError");
+                })
+                .finally(() => {
+                    usernameCheckStub.restore();
+                });
+        });
+
+        it("should be safe against MongoDB attack", () => {
+            return agent.get("/check_username")
+                .query({
+                    "username[$gt]": "",
+                })
+                .then(res => {
+                    assert.equal(res.statusCode, 400);
+                    assert.equal(res.body.errorID, "invalidUsernameToCheck");
+                });
+        });
+
+        it("should only process the first username passed to it, if multiple were passed in", () => {
+            return agent.get("/check_username")
+                .query({
+                    "username": [TEST_USER, "someone_else"],
+                })
+                .then(res => {
+                    assert.equal(res.statusCode, 200);
+                    const message = res.body.message;
+                    assert.isTrue(message.exists);
+                });
+        });
+
+        it("should throw an error if an empty array is passed to check_username", () => {
+            return agent.get("/check_username")
+                .query({
+                    "username": [],
+                })
+                .then(res => {
+                    assert.equal(res.statusCode, 400);
+                    assert.equal(res.body.errorID, "noUsernameToCheck");
+                });
+        });
+
+        afterEach(() => {
+            usersCollection = db.collection("users");
+            usersCollection.deleteMany({});
+            agent.close();
+        });
+        after(() => {
+            mongod.stop();
+            databaseOps.closeDatabaseClient();
+        });
+    });
+});
