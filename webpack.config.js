c2f85aa47ba851b0962d97b7722a00fe1a3f0582
Coteh <3276350+Coteh@users.noreply.github.com>
2021-08-11 00:15:39 -0400

    Add Sentry to the frontend

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 var path = require("path");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+const { DefinePlugin } = require("webpack");
 
 module.exports = {
     mode: "production",
+    devtool: "source-map",
     entry: {
         'assets/js/home.bundle': ['./assets/js/main.js', './assets/js/file-upload.js', './assets/js/util-frontend.js', './assets/js/image-util-frontend.js'],
         'assets/js/image.bundle': ['./assets/js/main.js', './assets/js/image.js', './assets/js/util-frontend.js'],
@@ -32,6 +36,24 @@ module.exports = {
                     to: 'favicon.ico'
                 },
             ],
+        }),
+        new SentryWebpackPlugin({
+            // sentry-cli configuration
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            org: process.env.SENTRY_ORG,
+            project: "simpleimage",
+            release: "simpleimage@" + process.env.npm_package_version,
+            // rewrite "./public/assets/js" prefix into "~/assets/js" so Sentry can detect the source maps
+            urlPrefix: "~/assets/js",
+
+            // webpack-specific configuration
+            include: ["./public/assets/js"],
+            ignore: ["node_modules", "webpack.config.js"],
+        }),
+        new DefinePlugin({
+            "SI_VERSION": JSON.stringify(process.env.npm_package_version),
+            "NODE_ENV": JSON.stringify(process.env.NODE_ENV) || "development",
+            "SENTRY_DSN": JSON.stringify(process.env.SENTRY_DSN),
         }),
     ],
 };
